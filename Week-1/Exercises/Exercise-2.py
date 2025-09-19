ccc526f45c3f7a24fa51b159cf4f5b5788c38b61
katarina <litricin.katarina3@gmail.com>
2025-09-19 21:50:04 +0200

    Added answers to Exercises 2

--- Week-1/Exercises/Exercise-2.py
+++ Week-1/Exercises/Exercise-2.py
@@ -14,10 +14,11 @@ Task:
 Write a for-loop that prints out the following line 20 times:
  All work and no play makes Jack a dull boy.
 """
-
 print("Exercise 2.1")
 
-pass
+for i in range(20):
+    print("All work and no play makes Jack a dull boy.")
+
 
 print("---")
 """
@@ -30,7 +31,8 @@ Write a for-loop that prints out the numbers from 0 up to 5 inclusive.
 
 print("Exercise 2.2")
 
-pass
+for i in range(6): 
+    print(i)
 
 print("---")
 
@@ -45,7 +47,8 @@ Write a for-loop that prints out the EVEN numbers from 2 up to 8 inclusive.
 
 print("Exercise 2.3")
 
-pass
+for i in range(2, 9, 2): 
+    print(i)
 
 print("---")
 
@@ -59,7 +62,8 @@ Now write another loop to print 9 through 0 (i.e., backwards).
 
 print("Exercise 2.4")
 
-pass
+for i in range(9, -1, -1):  
+    print(i)
 
 print("---")
 
@@ -81,7 +85,9 @@ Write code that prints out the following sequence:
 
 print("Exercise 2.5")
 
-pass
+for i in range(1, 9): 
+    print("z" * i)
+
 
 print("---")
 
@@ -106,7 +112,11 @@ of a simple print-statement.
 
 print("Exercise 2.6")
 
-pass
+for i in range(1, 6):      
+    for j in range(1, i+1):
+        print(j, end="")   # print numbers on the same line
+    print()                
+
 
 print("---")
 
@@ -135,7 +145,11 @@ your code needs to carry out for a certain value of rows.
 print("Exercise 2.7")
 
 rows = 5
-pass
+
+for i in range(1, rows + 1):
+    spaces = " " * (rows - i)          
+    circles = "o" * (2 * i - 1)        
+    print(spaces + circles)
 
 print("---")
 
@@ -163,6 +177,9 @@ on how to align the numbers.
 
 print("Exercise 2.8")
 
-pass
+for i in range(1, 11):        # outer loop for rows
+    for j in range(1, 11):    # inner loop for columns
+        print(i * j, end=" ") # print product, stay on same line
+    print()                   # move to the next line
 
 print("---")
